d133612f957beaf0cc20458d45c4331676a30612
scott306lr <scott306lr@gmail.com>
2022-09-08 16:10:16 +0800

    additional analysis

--- src/model_analysis/visualize.py
+++ src/model_analysis/visualize.py
@@ -3,8 +3,22 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 import numpy as np
 from matplotlib import pyplot as plt
+
+from ..post_quant.find_distribution import find_layer_dist, find_layers_scale
 from .utils import simulate_input
 from ..post_quant.utils import HookHandler, get_linear_layers 
+
+def add_value_labels(ax, skip_cnt, precision=2):
+    line = ax.lines[0]
+    skip = 0
+    for x_value, y_value in zip(line.get_xdata(), line.get_ydata()):
+        if skip % skip_cnt == 0:
+            label = format(y_value, f'.{precision}f')
+            ax.annotate(label, (x_value, y_value), xytext=(0, 5), fontsize=14, 
+                textcoords="offset points", ha='center', va='bottom')
+            skip = 1
+        else: 
+            skip += 1
 
 def my_boxplot(data, labels, name, ax, total=None, sep_interval=None):
     if ax is None:
@@ -47,27 +61,6 @@ def flat_act_func(org_val, output):
 def act_dist(model, start, end, show_layers=None, name="Activation Distribution from Gamma_1/Gamma_2 for Each Layer", ax=None):
     model_layers = []
     for i in range(start, end+1):
-        todo_layer = model.blocks[i] # todo_layer = getattr(model, f"layer{i}")
-        model_layers.append(get_linear_layers(todo_layer, specify_names=show_layers, prefix=f"{i}-"))
-
-    activations = {}
-    hook_handler = HookHandler()
-    hook_handler.create_apply_hook(flat_act_func, activations, model_layers)
-    model(simulate_input())
-    hook_handler.remove_hook()
-    
-    data = []
-    labels = []
-    for layer in model_layers:
-        for n, m in layer:
-            data.append(activations[n])
-            labels.append(n)
-  
-    my_boxplot(data, labels, name, ax, total=len(labels), sep_interval=(len(labels) // (end-start+1)))
-
-def act_dist(model, start, end, show_layers=None, name="Activation Distribution from Gamma_1/Gamma_2 for Each Layer", ax=None):
-    model_layers = []
-    for i in range(start, end+1):
         todo_layer = model.blocks[i]
         model_layers.append(get_linear_layers(todo_layer, specify_names=show_layers, prefix=f"{i}-"))
 
@@ -85,3 +78,28 @@ def act_dist(model, start, end, show_layers=None, name="Activation Distribution 
             labels.append(n)
   
     my_boxplot(data, labels, name, ax, total=len(labels), sep_interval=(len(labels) // (end-start+1)))
+
+def scale_plot(model, start, end, show_layers=None, name="Activation Distribution from Gamma_1/Gamma_2 for Each Layer", ax=None):
+    model_layers = []
+    for i in range(0, 24):
+        todo_layer = model.blocks[i]
+        model_layers.append(get_linear_layers(todo_layer, specify_names=show_layers, prefix=f'{i}-')) # cross-channel sublayer only
+    
+    layers_scale = find_layers_scale(model, model_layers)
+    
+    data = []
+    labels = []
+    for layer in model_layers:
+        for n, m in layer:
+            data.append(layers_scale[n]["scale"])
+            labels.append(n)
+    
+
+    ax.set_title(name, size=30)
+    ax.tick_params(labelrotation=30)
+    for label in (ax.get_xticklabels() + ax.get_yticklabels()):
+        label.set_fontsize(16)
+    ax.plot(labels, data)
+
+    return labels, data
+    # my_boxplot(data, labels, name, ax, total=len(labels), sep_interval=(len(labels) // (end-start+1)))
--- src/post_quant/find_distribution.py
+++ src/post_quant/find_distribution.py
@@ -1,4 +1,6 @@
 import torch
+
+from ..utils.quantization_utils.quant_utils import signed_max_bits
 from .utils import HookHandler
 from ..utils.data_utils import *
 
@@ -10,15 +12,15 @@ class StdMean(object):
 def std_mean_func(org_val, output):
     momentum = 0.99
     new_std_mean = torch.std_mean(output.detach(), dim=[0, 1], unbiased=False)
-    if org_val is not None:
-        return {
-            "std":  org_val*momentum + new_std_mean[0]*(1 - momentum), 
-            "mean": org_val*momentum + new_std_mean[1]*(1 - momentum)
-        }
-    else:
+    if org_val is None:
         return {
             "std":  new_std_mean[0], 
             "mean": new_std_mean[1]
+        }
+    else:
+        return {
+            "std":  org_val["std"] *momentum + new_std_mean[0]*(1 - momentum), 
+            "mean": org_val["mean"]*momentum + new_std_mean[1]*(1 - momentum)
         }
 
 def find_layer_dist(model, model_layers):
@@ -29,7 +31,7 @@ def find_layer_dist(model, model_layers):
     hook_handler.create_apply_hook(std_mean_func, layers_dist, model_layers)
 
     print("Loading a small piece of training data...")
-    data_loader = getTrainData(dataset='imagenet', path="/mnt/disk1/imagenet", batch_size=64, data_percentage=0.001)
+    data_loader = getTrainData(dataset='imagenet', path="E:\datasets\imagenet", batch_size=64, data_percentage=0.001)
     
     print("Calibrating...")
     calibrate(data_loader, model)
@@ -38,3 +40,46 @@ def find_layer_dist(model, model_layers):
     hook_handler.remove_hook()
 
     return layers_dist
+
+def calc_quant_scale(min_val, max_val, bits):
+        sat_val = torch.max(min_val.abs(), max_val.abs())
+        n = signed_max_bits(bits)
+        if sat_val == 0 : 
+            sat_val = n 
+        scale = torch.clamp(sat_val, min=1e-8) / n
+        return scale
+
+def scale_func(org_val, output):
+    averaging_constant = 0.1
+    x = output.detach().cpu()
+    x = x.to(torch.float32)
+
+    if org_val is None:
+         min_val, max_val = torch.aminmax(x)
+    else:
+        min_val_cur, max_val_cur = torch.aminmax(x)
+        min_val = org_val["min_val"] + averaging_constant * (min_val_cur - org_val["min_val"])
+        max_val = org_val["max_val"] + averaging_constant * (max_val_cur - org_val["max_val"])
+    return {
+        "min_val": min_val,
+        "max_val": max_val,
+        "scale"  : calc_quant_scale(min_val, max_val, 8)
+    }
+
+def find_layers_scale(model, model_layers):
+    layers_scale = {}
+
+    print("Creating hooks...")
+    hook_handler = HookHandler()
+    hook_handler.create_apply_hook(scale_func, layers_scale, model_layers)
+
+    print("Loading a small piece of training data...")
+    data_loader = getTrainData(dataset='imagenet', path="E:\datasets\imagenet", batch_size=32, data_percentage=0.001)
+    
+    print("Calibrating...")
+    calibrate(data_loader, model)
+    
+    print('Removing hooks...')
+    hook_handler.remove_hook()
+
+    return layers_scale
